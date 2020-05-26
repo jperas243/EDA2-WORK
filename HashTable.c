7535da07edf4878b04dff78a82b5071b5f948f0c
jperas243 <joaop.pereira@sapo.pt>
2020-05-26 23:15:21 +0100

    update ganda marco fig

--- HashTable.c
+++ HashTable.c
@@ -13,8 +13,8 @@ typedef struct hashTable{
 
 typedef struct student {
 
-    char *student_id; 
-    char *student_country;
+    char student_id[7]; 
+    char student_country[3];
     bool student_done;
     bool student_left;
     bool invalid_position;
@@ -22,25 +22,17 @@ typedef struct student {
 } student_t;
 
 HashTable_t* new_HashTable(char file_name[21]){
+    
     HashTable_t *new = malloc(sizeof(HashTable_t));
     new->maxSize = SIZE;
-    if(fopen(file_name, "r+") == NULL)
+    new->ref = fopen(file_name, "r+");
+    if( new->ref== NULL)
         new->ref = fopen(file_name, "w+");
 
     return new;
     
 }
 
-student_t *new_student(char *id, char *country){
-    student_t *new = malloc(sizeof(student_t));
-    new->student_id = id;
-    new->student_country = country;
-    new->student_done = false;
-    new->student_left = false;
-    new->invalid_position = true;
-
-    return new;
-}
 
 int hashCode(const char *str) {
     int hash = 0;
@@ -51,8 +43,6 @@ int hashCode(const char *str) {
 
 int position_process(char *id, char *country)
 {
-
-
 
 /*
     if()
@@ -69,18 +59,47 @@ int aux_func(int n)
     return n*n;
 }
 
-bool insert_hashtable(char *id, char *cod) {
+bool insert_hashtable(HashTable_t table,char *id, char *cod) {
 
+    //fread();
     return false;  
   
 }
 
+int remove_hashtable(HashTable_t table,char *id)
+{
+    return 0;
+}
+
+int find_hashtable(HashTable_t table,char *id)
+{
+    return 0;
+}
+
 int main(int argc, char const *argv[])
 {
-    HashTable_t *alunos = new_HashTable("teste.dat");
+    //HashTable_t *alunos = new_HashTable("teste.dat");
+    FILE *ref = fopen("teste.dat","wb+");
 
+    student_t aluno1;
+    strcpy(aluno1.student_id,"ABFC12");
+    student_t aluno2;
+    strcpy(aluno2.student_id,"ABFC30");
+
+
+
+    int offset = sizeof(struct student)*0;
+    fseek(ref, offset, SEEK_SET);
     
+    fwrite(&aluno1,sizeof(struct student),1,ref);
 
+
+    offset = sizeof(struct student)*0;
+    fseek(ref, offset, SEEK_SET);
+
+    fread(&aluno2, sizeof(struct student), 1, ref);
+
+    printf("%s\n",aluno2.student_id);
 
 
     return 0;
